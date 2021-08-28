2e0bcec2e70d5c6d186acb15f6764b48777fc1a6
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2021-08-29 00:02:40 +0530

    Added a barebones outline of landing page.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 export default function Landing() {
 	return (
-		<div className='w-screen flex flex-row'>
-			<div>sup bruh</div>
-			<div>photu</div>
+		<div className='w-screen flex flex-row justify-center items-center'>
+			{/* text & buttons */}
+			<div>
+				{/* Hi, I'm Shameek Kumar Baranwal / Front-End Web Developer / Freelancer / CS Sophomore at BITS Pilani */}
+				<div className=''>
+					<p>Hi, I'm</p>
+					<p>Shameek Kumar Baranwal</p>
+				</div>
+				{/* [Know More] [View Resume] */}
+				<div className=''>
+					<button>Know More</button>
+					<button>View Resume</button>
+				</div>
+			</div>
+			{/* Image */}
+			<div>
+				<img src='' alt='display picture' />
+			</div>
 		</div>
 	);
 }
