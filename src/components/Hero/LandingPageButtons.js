b7adf81fe51eee86ec0cec5edafcfe80cd802728
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2021-09-20 18:00:04 +0530

    Added Resume.

--- src/components/Hero/LandingPageButtons.js
+++ src/components/Hero/LandingPageButtons.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
+import Resume from "../../assets/Shameek's Resume.pdf";
+
 export function LandingPageButtons() {
 	return (
 		<div className='flex flex-row justify-center items-center transition-all duration-100 z-[1]'>
-			<a
-				className='transition-all duration-200 border-2 border-turq-600 bg-turq-600 px-3 py-2 mx-2 text-richblue-800 hover:bg-opacity-10 hover:border-seashell hover:border-opacity-20 hover:text-seashell rounded-md font-light hover:font-medium hover:text-lg'
-				href='#about'
-			>
+			<a className='primary-btn' href='#about'>
 				Know More
 			</a>
 			<a
-				className='transition-all duration-200 border-2 border-seashell px-3 py-2 mx-2 my-10 hover:border-opacity-10 rounded-md font-light hover:font-medium hover:text-lg'
-				href='#skillset'
+				className='secondary-btn'
+				href={Resume}
+				rel='noopener noreferrer'
+				target='_blank'
 			>
 				View Resume
 			</a>
