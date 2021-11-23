3d3b76d63ca95e2a815dfd5fdb5d00da066c1a0a
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2021-11-24 01:05:52 +0530

    Improved Button design.

--- src/components/General/PrimaryButton.js
+++ src/components/General/PrimaryButton.js
@@ -4,7 +4,7 @@ export default function SecondaryButton({ href, children }) {
 	return (
 		// <button className='' data-aos='fade-up'>
 		<a
-			className='transition-all duration-200 border-2 border-turq-600 bg-turq-600 px-3 py-2 mx-2 text-richblue-800 hover:bg-opacity-10 hover:border-seashell hover:border-opacity-20 hover:text-seashell rounded-md font-light hover:font-medium hover:text-lg'
+			className='transition-all duration-200 border-0 border-turq-600 bg-turq-600 px-3 py-3 mx-2 text-richblue-800 hover:bg-opacity-10 hover:rounded-lg hover:border-seashell hover:text-seashell rounded-md font-light hover:font-medium hover:text-lg'
 			href={href}
 			target='_blank'
 			rel='noopener noreferrer'
--- src/components/General/SecondaryButton.js
+++ src/components/General/SecondaryButton.js
@@ -4,7 +4,7 @@ export default function SecondaryButton({ href, children }) {
 	return (
 		// <button data-aos='fade-up'>
 		<a
-			className='transition-all duration-200 border-2 border-seashell px-3 py-2 mx-2 my-10 hover:border-opacity-10 rounded-md font-light hover:font-medium hover:text-lg'
+			className='transition-all duration-200 border-2 border-seashell px-3 py-3 mx-2 my-10 hover:border-opacity-0 bg-white bg-opacity-0 hover:bg-opacity-10 rounded-md font-light hover:font-medium hover:text-lg'
 			href={href}
 			target='_blank'
 			rel='noopener noreferrer'
